f18e07128aa4327a5d9f6e81bd80f250cfbb5fb6
MariaKlimkin <maria1klimkin@gmail.com>
2018-04-07 13:05:09 +0300

    final MainAux + documentation

--- RPSMainAux.cpp
+++ RPSMainAux.cpp
@@ -1,10 +1,33 @@
 
 #include "RPSMainAux.h"
 
+/**
+ * In case there is a winner (or two winners) the function checks if the reason for the winning is
+ * because all opponents flags are eaten or because all moving tool are eaten, and updates the reason.
+ *
+ * @param game - the current game
+ * @param reason - will be 1 if all flags of the opponent are captured
+ *                 or 2 if all moving pieces of the opponent are eaten
+ *
+ */
+void RPSMainAuxCheckAndUpdateReasonForWinner(RPSGame* game, int &reason){
+    if (game->player1ToolCounters["F"] == F || game->player1ToolCounters["f"] == F)
+        reason = 1; // "all flags of the opponent are captured"
+    else
+        reason = 2; // "all moving pieces of the opponent are eaten"
+}
 
+
+/**
+ * checks if at this point of the game there is winner, and updates the reason and winner parameters
+ * @param winner 0, 1 or 2
+ * @param reason 1,2,3,4 or 5
+ * @param game - the current game
+ */
 void RPSMainAuxFinalCheckOfGameBoard(int &winner, int &reason, RPSGame* game){
-    if (game->RPSGameCheckIfPlayer1Lose() && game->RPSGameCheckIfPlayer2Lose()) // both wins
+    if (game->RPSGameCheckIfPlayer1Lose() && game->RPSGameCheckIfPlayer2Lose()) { // both wins
         winner = 0;
+    }
     else if (game->RPSGameCheckIfPlayer1Lose()) //player 2 wins
         winner = 2;
     else if (game->RPSGameCheckIfPlayer2Lose())// player 1 wins
@@ -14,74 +37,45 @@ void RPSMainAuxFinalCheckOfGameBoard(int &winner, int &reason, RPSGame* game){
         reason = 3; // "A tie - both moves input files done without a winner"
         return;
     }
-    if (game->player1ToolCounters["F"] == F || game->player1ToolCounters["f"] == F)
-        reason = 1; // "all flags of the opponent are captured"
-    else
-        reason = 2; // "all moving pieces of the opponent are eaten"
-}
-
-
-int RPSMainAuxGameHandler(){
-
-    RPSGame* newGame = new RPSGame();
-    int param1;
-    int param2;
-    int reason; //"reason" for output file
-                // 1=all flags of the opponent are captures
-                // 2= , 3=, 4=, 5=, 6=bad moves input file for player...
-    int winner; // 0=tie, 1=player1, 2=player2
-    bool flagsEaten = false;
-
-    // checking the board game input files
-    param1 = newGame->RPSGameInitFileCheck("player1.rps_board", 1, newGame->player1ToolCounters);
-    param2 = newGame->RPSGameInitFileCheck("player2.rps_board", 2, newGame->player2ToolCounters);
-    if (param1 != 0 && param2 != 0){ //at least one of the files is bad
-        RPSMainAuxUpdateWinner(param1, param2, winner);
-        RPSMainAuxMakeOutputFile(4, param1, param2, winner, newGame);
-        return 0;
-    }
-    // input files are valid, now before setting the moves we want to check if maybe there is a winner...
-    if (newGame->RPSGameCheckIfPlayer1Lose() || newGame->RPSGameCheckIfPlayer2Lose()){// someone wins or both wins
-        RPSMainAuxFinalCheckOfGameBoard(winner, reason, newGame);
-        RPSMainAuxMakeOutputFile(reason, 0, 0, winner, newGame);
-        return 0;
-    }
-    // now lets read moves files
-    param1 = newGame->RPSGameMoveFileCheck("player1.rps_moves", "player2.rps_moves", param2);
-    if (param1 == 0){// tie, both moves files don't exist
-        winner = 0;
-        RPSMainAuxMakeOutputFile(3, 0, 0, winner, newGame);
-        return 0;
-    }
-    else if (param1 == 1 || param1 == 2){ //"bad moves input file"
-        if (param1 == 1)
-            winner = 2;
-        else
-            winner = 1;
-        RPSMainAuxMakeOutputFile(5, param1, param2, winner, newGame); // here param2 == bad line number
-        return 0;
-    }
-    else{ //param1=3 , moves files are OK, we need to check if there is a winner
-        RPSMainAuxFinalCheckOfGameBoard(winner, reason, newGame);
-        RPSMainAuxMakeOutputFile(reason, param1, param2, winner, newGame);
-        return 0;
-    }
-
-
-
+    RPSMainAuxCheckAndUpdateReasonForWinner(game, reason);
 
 }
 
 
+/**
+ * Updates the winner according to the value of param1 and param2. This is done after
+ * reading the positioning input files.
+ *
+ * @param param1
+ * @param param2
+ * @param winner
+ */
 void RPSMainAuxUpdateWinner(int param1, int param2, int &winner){
-    if (param1 != 0) //bad input file for player 1
+    if (param1!=0 && param2!=0)
+        winner = 0; //tie
+    else if (param1 != 0) //bad input file for player 1
         winner = 2;
     else // bad input file for player 2
         winner = 1;
 }
 
 
+int RPSMaimAuxTerminateTheGame(RPSGame* game){
+    delete(game);
+    return 0;
+}
 
+
+/**
+ * This function is called after reading the moves input files, and when one of the lines in one of
+ * the files is invalid. Then it updates the loser (according to the winner), and the "bad" line (number of invalid line).
+ *
+ * @param winner
+ * @param loser
+ * @param param1
+ * @param param2
+ * @param badLine
+ */
 void RPSMainAuxUpdateLoserAndBadLine(int winner, int &loser, int param1, int param2 , int &badLine){
     if (winner == 1) {
         loser = 2;
@@ -94,6 +88,15 @@ void RPSMainAuxUpdateLoserAndBadLine(int winner, int &loser, int param1, int par
 }
 
 
+/**
+ * Writes the output file "rps.output"
+ *
+ * @param reason
+ * @param param1
+ * @param param2
+ * @param winner
+ * @param game
+ */
 void RPSMainAuxMakeOutputFile(int reason, int param1, int param2, int winner, RPSGame* game){
     ofstream fout("rps.output");
     int loser;
@@ -121,11 +124,88 @@ void RPSMainAuxMakeOutputFile(int reason, int param1, int param2, int winner, RP
     else if (reason == 3)
         fout << "A tie - both Moves input files done without a winner" << endl;
 
-    else if (reason == 5){
+    else{ //(reason == 5)
         RPSMainAuxUpdateLoserAndBadLine(winner, loser, param1, param2, badLine);
-        fout << "Bad Moves input file for player " << loser << " - line" << param2 << endl;
+        fout << "Bad Moves input file for player " << loser << " - line " << param2 << endl;
     }
 
+    cout << endl;
+    // printing the game board state
+    for (int i=0 ; i<N ; i++){
+        for (int j=0 ; j<M ; j++){
+            fout << game->board[i][j];
+        }
+        fout << endl;
+    }
+    fout.close();
 }
+
+
+/**
+ * The handler of the game
+ *
+ * @return 0
+ */
+int RPSMainAuxGameHandler(){
+
+    RPSGame* newGame = new RPSGame();
+    int param1;
+    int param2;
+    int reason; // the "reason" for output file
+                // 1=all flags of the opponent are captures
+                // 2=All moving PIECEs of the opponent are eaten
+                // 3=A tie - both Moves input files done without a winner
+                // 4=bad positioning input file for onr player or both
+                // 5=bad moves input file for some player
+    int winner; // 0=tie, 1=player1 wins, 2=player2 wins
+    bool flagsEaten = false;
+
+    // checking the board game input files
+    param1 = newGame->RPSGameInitFileCheck("player1.rps_board", 1, newGame->player1ToolCounters);
+    param2 = newGame->RPSGameInitFileCheck("player2.rps_board", 2, newGame->player2ToolCounters);
+
+    if (param1!=0 || param2!=0){ //at least one of the positioning input files is bad
+        RPSMainAuxUpdateWinner(param1, param2, winner);
+        RPSMainAuxMakeOutputFile(4, param1, param2, winner, newGame);
+        return RPSMaimAuxTerminateTheGame(newGame);
+    }
+
+    // input files are valid, now before setting the moves we want to check if maybe there is already a winner...
+    if (newGame->RPSGameCheckIfPlayer1Lose() || newGame->RPSGameCheckIfPlayer2Lose()){// someone wins or both wins
+        RPSMainAuxFinalCheckOfGameBoard(winner, reason, newGame);
+        RPSMainAuxMakeOutputFile(reason, 0, 0, winner, newGame);
+        return RPSMaimAuxTerminateTheGame(newGame);
+    }
+
+    // now lets read moves files
+    param1 = newGame->RPSGameMoveFileCheck("player1.rps_moves", "player2.rps_moves", param2);
+    if (param1 == -1){// tie, both moves files don't exist
+        winner = 0;
+        RPSMainAuxMakeOutputFile(3, 0, 0, winner, newGame);
+    }
+    else if (param1 == 1 || param1 == 2){ //"bad moves input file"
+        if (param1 == 1)
+            winner = 2;
+        else
+            winner = 1;
+        RPSMainAuxMakeOutputFile(5, param1, param2, winner, newGame); // here param2 == bad line number
+    }
+    else{ //param1=0 , moves files are OK, we need to check if there is a winner
+        RPSMainAuxFinalCheckOfGameBoard(winner, reason, newGame);
+        RPSMainAuxMakeOutputFile(reason, param1, param2, winner, newGame);
+    }
+
+    return RPSMaimAuxTerminateTheGame(newGame);
+}
+
+
+
+
+
+
+
+
+
+
 
 
--- RPSMainAux.h
+++ RPSMainAux.h
@@ -12,14 +12,20 @@ using namespace std;
 #ifndef EX1_RPSMAINAUX_H
 #define EX1_RPSMAINAUX_H
 
-void RPSMainAuxFinalCheckOfGameBoard(int &winner, int &reason, RPSGame* game);
 
-int RPSMainAuxGameHandler();
+void RPSMainAuxCheckAndUpdateReasonForWinner(RPSGame* game, int &reason);
+
+void RPSMainAuxFinalCheckOfGameBoard(int &winner, int &reason, RPSGame* game);
 
 void RPSMainAuxUpdateWinner(int param1, int param2, int &winner);
 
-void RPSMainAuxMakeOutputFile(int reason, int result1, int result2, int winner, RPSGame* game);
+int RPSMaimAuxTerminateTheGame(RPSGame* game);
 
 void RPSMainAuxUpdateLoserAndBadLine(int winner, int &loser, int param1, int param2 , int &badLine);
+
+void RPSMainAuxMakeOutputFile(int reason, int result1, int result2, int winner, RPSGame* game);
+
+int RPSMainAuxGameHandler();
+
 
 #endif //EX1_RPSMAINAUX_H
